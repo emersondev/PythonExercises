b301acf24869f3c75c21cf85cc655f6cad8d0c7d
Emerson Novais <emersonnovais93@gmail.com>
2023-08-22 20:23:48 -0300

    ex004

---
+++ ModuleOne/ex003.py
@@ -0,0 +1,4 @@
+value1 = float(input('Digite seu valor: '))
+value2 = float(input('Digite seu outro: '))
+total = value1 + value2
+print('A soma dos valores {} e {} equivalem a: {}'.format(value1, value2, total))
---
+++ ModuleOne/ex004.py
@@ -0,0 +1,5 @@
+value = input('digite algo: ')
+print('Qual é o tipo do dado? {}'.format(type(value)))
+print('Valor é numérico? {}'.format(value.isnumeric()))
+print('Valor é alfabético? {}'.format(value.isalpha()))
+print('Valor é alfanumérico? {}'.format(value.isalnum()))
