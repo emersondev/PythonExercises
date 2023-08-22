9fcbac38c2fb2588176b3cf83f4b65327c8abf24
Emerson Novais <emersonnovais93@gmail.com>
2023-08-22 04:11:53 -0300

    soft changes

--- ModuleOne/ex002.py
+++ ModuleOne/ex002.py
@@ -1,2 +1,2 @@
-name = input("Qual é o seu nome?, digite aqui: ")
-print("É um prazer te conhecer, {}!".format(name))
+name = input('Qual é o seu nome?, digite aqui: ')
+print('É um prazer te conhecer, {}!'.format(name))
