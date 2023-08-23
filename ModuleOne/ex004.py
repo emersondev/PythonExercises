f5e05051ee4c9ab1bbd10826c8dcacc4235800ed
Emerson Novais <emersonnovais93@gmail.com>
2023-08-23 12:28:05 -0300

    Update ex004.py

--- ModuleOne/ex004.py
+++ ModuleOne/ex004.py
@@ -3,3 +3,8 @@ print('Qual é o tipo do dado? {}'.format(type(value)))
 print('Valor é numérico? {}'.format(value.isnumeric()))
 print('Valor é alfabético? {}'.format(value.isalpha()))
 print('Valor é alfanumérico? {}'.format(value.isalnum()))
+print('Valor só tem espaço? {}'.format(value.isspace()))
+print('Valor é maiúsculo? {}'.format(value.isupper()))
+print('Valor é minúsculo? {}'.format(value.islower()))
+print('Valor é capitalizado? {}'.format(value.istitle()))
+
