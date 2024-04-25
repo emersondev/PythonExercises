cb06d19e52139ecfc44fbbbff10abebbd3a7badc
Emerson Novais <emersonnovais93@gmail.com>
2024-04-24 22:35:38 -0300

    aula 08

--- ModuleOne/Lessons/aula08a.py
+++ ModuleOne/Lessons/aula08a.py
@@ -2,8 +2,20 @@
 para fazer uma importação, somente seguir o padrão "from BIBLIOTECA import FUNÇÃO, FUNÇÃO2"
 """
 
+# raíz quadrada
 from math import sqrt
 
 num = int (input('Digite um número: '))
 raiz = sqrt(num)
 print('A raiz de {} é igual a {:.2f}'.format(num,raiz))
+
+
+"""
+ceil ------> arredonda pra cima
+floor -----> arredonda pra baixo
+truc ------> vai trucar o número (numero inteiro)
+pow -------> potência
+sqrt ------> raíz quadrada
+factorial -> fatorial
+"""
+
